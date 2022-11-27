5c96c46a8bc956a7a3049dd736e5c2db38e11847
Anurag Verma <118360572+anuragvermaoo7@users.noreply.github.com>
2022-11-27 18:43:20 +0530

    Create Hierarchical.cpp

---
+++ Inheritance/Hierarchical.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class A 
+{
+    public:
+
+    void func1()
+    {
+        cout << "Inside Funcion 1" << endl;
+    }
+
+};
+
+class B: public A 
+{
+    public:
+
+    void func2() {
+        cout << "Inside Funcion 2" << endl;
+    }
+
+};
+
+class C: public A
+{
+    public:
+
+    void func3() {
+        cout << "Inside Funcion 3" << endl;
+    }
+
+};
+
+int main() {
+
+    cout<<"CLASS A"<<endl;
+    A object1;
+    object1.func1();
+    
+    cout<<"CLASS B"<<endl;
+    B object2;
+    object2.func1();
+    object2.func2();
+
+    cout<<"CLASS C"<<endl;
+    C object3;
+    object3.func1();
+    object3.func3();
+
+    return 0;
+}
