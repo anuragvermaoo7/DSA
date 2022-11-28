3398b0b5ece2aeb09316bdc322fb25cc77c9e462
Anurag Verma <118360572+anuragvermaoo7@users.noreply.github.com>
2022-11-28 10:40:21 +0530

    Update Singly Linked List.cpp

--- Linked List/Singly Linked List.cpp
+++ Linked List/Singly Linked List.cpp
@@ -1,21 +1,62 @@
 #include<iostream>
 using namespace std;
-class Node
-{
+
+class Node {
+
     public:
 
     int data;
     Node* next;
 
-    Node(int a)
+    Node(int data)
     {
-        this -> data = a;
+        this -> data = data;
         this -> next = NULL;
+    }
+
+    void insertAtHead(Node* &head, int d)
+    {
+        Node* temp = new Node(d);
+        temp -> next = head;
+        head = temp;
+    }
+
+    void insertAtTail(Node* &tail, int d)
+    {
+        Node* temp = new Node(d);
+        tail -> next = temp;
+        tail  = temp;
+    }
+
+    void print(Node* &head)
+    {
+
+        Node* temp = head;
+
+        while(temp != NULL )
+        {
+            cout << temp -> data << " ";
+            temp = temp -> next;
+        }
+    cout << endl;
     }
 };
 int main()
 {
-    Node* Node1 = new Node(10);
-    cout<< Node1 -> data;
+
+    Node* node1 = new Node(10);
+    //cout << node1 -> data << endl;
+    // cout << node1 -> next << endl;
+    
+    Node* head = node1; 
+    Node* tail = node1;
+    print(head);
+
+    insertAtTail(tail, 12);
+    print(head);
+    
+    insertAtTail(tail, 15);
+    print(head);
+
     return 0;
-}
+}    
